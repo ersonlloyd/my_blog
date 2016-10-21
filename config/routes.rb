a3eff6cb5d1eb7e9814bcb6adb892e0d850995cf
@ersonlloyd <erson@proudcloud.io>
2016-10-21 16:06:21 +0800

    Static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
+
 Rails.application.routes.draw do
-  get 'static_pages/home'
 
-  get 'static_pages/articles'
 
-  get 'static_pages/help'
+  root 'static_pages#home'
+
+  get '/home', to: 'static_pages#home'
+
+  get '/article', to: 'static_pages#articles'
+
+  get '/help', to: 'static_pages#help'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
