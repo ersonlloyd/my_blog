ded6d52bbbb234ad0c88cbc5600590adb5bfbe29
@ersonlloyd <erson@proudcloud.io>
2016-10-25 14:10:35 +0800

    Finish sign up

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,13 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def help
+  end
+
+  def article
+  end
+
+  def contacts
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
+
 Rails.application.routes.draw do
+
+  resources :users
+
+  get 'users/new'
+
+  root 'static_pages#home'
+
+  get '/home', to: 'static_pages#home'
+
+  get '/article', to: 'static_pages#articles'
+
+  get '/help', to: 'static_pages#help'
+
+  get '/signup', to: 'users#new'
+
+  post '/signup',  to: 'users#create'  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
